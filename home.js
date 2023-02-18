f501637d9e5a6347fd2638d3a3f13e2e5e874c0d
PAnkita123 <malviyaankita901@gmail.com>
2023-02-18 15:31:07 +0530

    commit

--- home.js
+++ home.js
@@ -1,1 +1,1 @@
-changepercentageoff(80);
+// changepercentageoff(80);
--- javascript.js
+++ javascript.js
@@ -2,3 +2,12 @@
 //     document.getElementById("percentage_off").textContent=percentage+"% off";
 // }
 
+// this will change the fontweight through js
+// const header = document.getElementById('percentage_off');
+// header.style.fontWeight ="100";
+
+const button = document.getElementById('sign-in');
+button.addEventListener('click',function(){
+    console.log("click")});
+
+
