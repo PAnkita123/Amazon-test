0f87c2c090026e1388a5bfe0665bdd5d3aeeeebd
PAnkita123 <malviyaankita901@gmail.com>
2023-03-04 08:47:15 +0530

    commit

--- script.js
+++ script.js
@@ -1,15 +1,15 @@
 // Function that is called when the form is submitted
 function register(event) {
-      // Prevent the default form submission behavior
+    // Prevent the default form submission behavior
     event.preventDefault();
     // Get the values of the input fields
     var userName = document.getElementById("userName").value;
     var userNumber = document.getElementById("userNumber").value;
     var userEmail = document.getElementById("userEmail").value;
     var userPassword = document.getElementById("userPassword").value;
-      // Create an object with the user data
+    // Create an object with the user data
     var userData = { name: userName, number: userNumber, email: userEmail, password: userPassword }
-     // Retrieve the existing user data from local storage or create an empty array
+    // Retrieve the existing user data from local storage or create an empty array
     dataFromLS = JSON.parse(localStorage.getItem("userData")) || [];
     console.log(dataFromLS, "dataFromLS")
 
@@ -19,14 +19,14 @@ function register(event) {
         if (dataFromLS[i].email === userEmail) {
             flag = true;
         }
-    // If the email is already present, show an alert message and don't save the user data    
+        // If the email is already present, show an alert message and don't save the user data    
     } if (flag === true) {
         alert("email already present, please use another email!");
     } // If the password is less than 6 characters, show an alert message and don't save the user data
     else if (userPassword.length < 6) {
         alert("Password must be 6 characters!");
     }// If any field is empty, show an alert message and don't save the user data
-     else if (!userName || !userNumber || !userEmail || !userPassword) {
+    else if (!userName || !userNumber || !userEmail || !userPassword) {
         alert("Please fill in all fields!");
     } // Otherwise, save the user data to local storage and redirect to the login page
     else {
@@ -41,5 +41,30 @@ function register(event) {
     }
 }
 
+function login(event) {
+    event.preventDefault();
+    var userEmail = document.getElementById("email").value;
+    var userPassword = document.getElementById("password").value;
 
+    var dataFromLS = JSON.parse(localStorage.getItem('userData'));
+
+    var flag = false;
+
+    for (var i = 0; i < dataFromLS.length; i++) {
+        if (dataFromLS[i].email === userEmail && dataFromLS[i].password === userPassword)
+            flag = true;
+
+    }
+    if (flag === true) {
+        document.getElementById("email").value = '';
+        document.getElementById("password").value = '';
+        alert("You are logged in successfully!");
+        window.location.href = "./index.html";
+        
+    } 
+    else {
+        alert("Wrong credentials entered!Please use correct email and password!");
+    }
+
+}
 
